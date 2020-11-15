136a92c41256753c0bf5565a1852491d3a42ee9e
Lexirem <me.vallribera@gmail.com>
2020-11-15 20:26:19 +0100

    hasta iter 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import ListBeers from './pages/ListBeers';
+import RandomBeer from './pages/RandomBeer';
+import NewBeer from './pages/NewBeer';
+import { Link, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iron Beers</h1>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/list-beers" component={ ListBeers } />
+        <Route exact path="/new-beer" component={NewBeer} />
+        <Route exact path="/random-beer" component={RandomBeer} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+import ListBeers from './ListBeers';
+import RandomBeer from './RandomBeer';
+import NewBeer from './NewBeer';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <img src="https://user-images.githubusercontent.com/23629340/40706572-933439b8-63ee-11e8-8d65-538fb59f79ab.png" alt="" height="600px" />
+                <Link to='/list-beers'> <h1>All beers</h1> </Link>
+                <Link to='/new-beer'> <h1>New beers</h1></Link>
+                <Link to='/random-beer'> <h1>Random beers</h1></Link>
+            </div>
+        )
+    }
+}
+export default Home
